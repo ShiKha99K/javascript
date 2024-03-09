80d0d1dad8965011caea43e7f273d7e33f642a4c
ShiKha99K <146867083+ShiKha99K@users.noreply.github.com>
2024-03-09 17:54:52 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+//let score = null // after changing in number it gives value 0
+//let score = undefined// after changing in number it gives value NaN
+//let score = true // after changing in number it gives value 1
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInNUmber = Number(score);
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+//"" => false
+//"shikha" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
