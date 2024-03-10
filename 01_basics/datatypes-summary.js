446538b0cc0f429fb0b3a154e921bc5707e0da21
ShiKha99K <146867083+ShiKha99K@users.noreply.github.com>
2024-03-10 11:22:10 +0000

    Datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -33,3 +33,33 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction); // return function but originally its datatype is called 'object function'
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (for Primitive), Heap (for Non-Primitive)
+
+let myYoutubename = "shikhakumari"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+} 
+
+let userTwo = userOne
+
+userTwo.email = "Shikha@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// in stack, we get the copy of the orginal and
+// making changes in copy doesn't change the original one
+
+//in heap, we get the reference to the data so 
+// making changes in one will change the data for both since they are pointing to same data
